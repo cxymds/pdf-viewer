a583360793ddedbfac6d43c2cec3ec5f20f775a3
cxymds <cxymds@qq.com>
2025-01-17 17:43:08 +0800

    基本样式完成

---
+++ config.mjs
@@ -0,0 +1,26 @@
+export default [
+  {
+    // 文件名称
+    name: "Skyblock",
+    // 缩略图
+    pic: "https://skyblock.hypixel.net/",
+    // 文章标题
+    title: "https://skyblock.hypixel.net/favicon.ico",
+    // 文章表述
+    desc: "Skyblock is a Minecraft game where players can build their own island and earn resources and items to upgrade their island. Players can also earn levels and rank up to become a Skyblock Pro.",
+    // 文章pdf地址
+    url: "https://skyblock.hypixel.net/",
+  },
+  {
+    name: "Hypixel",
+    pic: "https://hypixel.net/",
+    title: "https://hypixel.net/favicon.ico",
+    url: "https://hypixel.net/",
+  },
+  {
+    name: "Hypixel API",
+    pic: "https://api.hypixel.net/",
+    title: "https://api.hypixel.net/favicon.ico",
+    url: "https://api.hypixel.net/",
+  },
+]
